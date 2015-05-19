5098146ab911a6c9b9bc9eda75a73c940a0f4326
trol <jorge.loza93@hotmail.com>
2015-05-19 10:14:34 +0300

    Introduciendo nueva linea,primero comint y luego subir

--- Pruebagithub/src/Principal.java
+++ Pruebagithub/src/Principal.java
@@ -4,6 +4,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Probando versiones");
+		System.out.println("Nueva linea");
 	}
 
 }
